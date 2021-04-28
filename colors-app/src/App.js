543e038e28680a4048a6c4a9eb0092415302161e
minal6066 <71309796+minal6066@users.noreply.github.com>
2021-04-28 23:45:46 +0530

    First commit

---
+++ colors-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React, { Component } from "react";
+import Palette from './Palette';
+import seedColors from './seedColors';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+         <Palette {...seedColors[4]} />
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
