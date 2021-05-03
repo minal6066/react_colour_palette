792d5d02e20c203777d4cf9158f34d7a6aa9dfe0
minal6066 <71309796+minal6066@users.noreply.github.com>
2021-05-03 23:37:21 +0530

    Added colorHelpers to generate colors

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import React, { Component } from "react";
 import Palette from './Palette';
 import seedColors from './seedColors';
+import {generatePalette} from './colorHelpers.js';
 
 class App extends Component {
   render(){
+    console.log(generatePalette(seedColors[4]));
     return (
       <div>
          <Palette {...seedColors[4]} />
